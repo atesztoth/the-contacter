31a4719578d88a91a059ae76df0df7b52c6b356c
Attila Tóth <atoth@loginet.hu>
2016-11-13 10:46:53 +0100

    playing with RequireJS

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,18 @@
 requirejs.config({
     baseUrl: 'bower',
+    // shim: {
+    //     'bootstrap': {'deps': ['jquery']} // jquery not supports amd (Oh shiet, from up to 1.7.* it does)
+    // },
     paths: {
+        bootstrap: 'bootstrap/dist/js/bootstrap',
         jquery: 'jquery/dist/jquery' // .min
     }
 });
 
-requirejs(['jquery'], function( $ ) {
-    // Main entry point. If you need to use for example jquery right now for somethings,
-    // do it like:
-    // require(['jquery'], function()....
-    console.log( $ ) // OK
-});
+// requirejs(['jquery'], function ($) {
+//     console.log($) // OK
+// });
+
+// requirejs(['jquery','bootstrap'], function($) {
+//     console.log($('body').modal());
+// });
