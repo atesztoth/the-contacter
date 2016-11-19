2f1a6f3440e9a09e19d5e2218ccbea66c4d7b104
Attila Toth <atoth@loginet.hu>
2016-11-19 14:02:43 +0100

    Getting started with Foundation grid sys.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,7 +4,8 @@ requirejs.config({
     //     'bootstrap': {'deps': ['jquery']} // jquery not supports amd (Oh shiet, from up to 1.7.* it does)
     // },
     paths: {
-        bootstrap: 'bootstrap/dist/js/bootstrap',
+        whatInput: 'what-input/what-input.min',
+        foundation: 'foundation-sites/dist/foundation.min',
         jquery: 'jquery/dist/jquery' // .min
     }
 });
@@ -13,6 +14,8 @@ requirejs.config({
 //     console.log($) // OK
 // });
 
-// requirejs(['jquery','bootstrap'], function($) {
-//     console.log($('body').modal());
-// });
+requirejs(['jquery', 'whatInput', 'foundation'], function ($) {
+    // $ is atteched to JQUERY! Evertyhing is ready to roll!
+    // console.log($);
+    console.log('If you see no error, that means everything is loaded correctly with its dependencies. Lets rock!');
+});
