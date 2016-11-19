6b3bd9a099a67cc8018df6e003e22650269a4f99
Attila Toth <atoth@loginet.hu>
2016-11-19 13:50:58 +0100

    Big update! Migrated to Foundation css framework, using a lot of new commands, using sass...

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,60 @@
 'use strict'
 
 var gulp = require('gulp');
-var gulpLess = require('gulp-less');
+var sass = require('gulp-sass');
 var bower = require('gulp-bower');
+var uglifycss = require('gulp-uglifycss');
 
-gulp.task('cmp-less', function () {
-    return gulp.src([
-        'public/assets/less/**/*.less',
-        '!public/assets/less/**/nocmp_*.less'])
-        .pipe(gulpLess()) // Converts Less to CSS with gulp-less
-        .pipe(gulp.dest('public/assets/css'))
+// gulp.task('cmp-less', function () {
+//     return gulp.src([
+//         'public/assets/less/**/*.less',
+//         '!public/assets/less/**/nocmp_*.less'])
+//         .pipe(gulpLess()) // Converts Less to CSS with gulp-less
+//         .pipe(gulp.dest('public/assets/css'))
+// });
+
+// I switched from less to sass because of Foundation.
+// Anyway, I just wanted to try out sass.
+
+gulp.task('default', function () {
+    console.log('Hello! Check gulpfile for available commands!');
 });
 
-gulp.task('bower-install', function () {
+gulp.task('sass:cmp', function () {
+    return gulp.src([
+        'public/assets/sass/**/*.scss',
+        '!public/assets/less/**/_*.scss']) // We are not compiling files starting with an underscore! I threat them internal files.
+        .pipe(sass({
+            style: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['cmp-sass']);
+});
+
+gulp.task('compress:css', ['sass:cmp'], function () {
+    gulp.src('public/assets/css/**/*.css')
+        .pipe(uglifycss({
+            "maxLineLen": 80,
+            "uglyComments": true
+        }))
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('bower:install', function () {
     return bower();
 });
 
-gulp.task('asset-watch', function () {
-    gulp.watch('public/assets/less/**/*.less', ['cmp-less']);
-    // Other watchers
-})
+gulp.task('say:finished', ['compress:css'], function () {
+    console.log('Installing finished, ready to roll!');
+});
+
+gulp.task('say:start', function () {
+    console.log('Hello! Getting the frontend ready...');
+});
+
+gulp.task('deploy', ['say:start'], function () {
+    gulp.start('say:finished');
+});
